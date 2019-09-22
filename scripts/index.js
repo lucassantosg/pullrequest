8dfe98e104e9a388e20175a04e283a2f3d67d23c
Lucas Gonçalves <lucas@gosd.com.br>
2019-09-22 01:02:33 -0300

    Bodymovin test

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json',
+  })
+
+function test(){
+  animation.anim.pause();
+}
